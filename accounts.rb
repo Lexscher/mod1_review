1a6404e5a70c2ec48ccab36d88fa114c41f8c58f
Alexander Schelchere <ascellie@gmail.com>
2020-06-02 10:43:33 -0400

    solved

--- accounts.rb
+++ accounts.rb
@@ -1,3 +1,17 @@
 class Account
- 
+    attr_reader :bank, :customer
+    attr_accessor :balance
+
+    @@all = []
+
+    def initialize(bank:, customer:, balance:)
+        @bank = bank
+        @customer = customer
+        @balance = balance
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
--- banks.rb
+++ banks.rb
@@ -1,3 +1,25 @@
 class Bank
-    
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+         @name = name
+         self.class.all << self
+    end
+ 
+    def self.all
+         @@all
+    end
+
+    def accounts
+        Account.all.select {|account| account.bank == self }
+    end
+
+    def customers
+        self.accounts.map { |account| account.customer }
+    end
+
+    def open_account(customer)
+        Account.new(bank: self, customer: customer, balance: 100)
+    end
+
 end
